84e82bd546540d49b66ed22c6703d0c24cf58ce8
stallonefennec <stalloneiv@gmail.com>
2025-01-12 22:08:41 +0800

    20250112-3

--- src/content/post/backup_memos.sh
+++ src/content/post/backup_memos.sh
@@ -4,15 +4,44 @@
 REPO_PATH="$HOME/.memos"  # 本地 Git 仓库路径
 REMOTE_REPO="git@github.com:stallonefennec/memos_db.git" # 远程 Git 仓库 URL
 LOG_FILE="$HOME/memos_backup.log"  # 日志文件路径
+CONTAINER_NAME="memos" # docker 容器名称
+DOCKER_COMPOSE_FILE="docker-compose.yml" # docker-compose 文件路径
 
+# 定义日志函数
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
+}
 # 获取当前日期和时间，用于日志记录
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
-echo "[$TIMESTAMP] Starting memos backup..." >> "$LOG_FILE"
+log "Starting memos backup..."
 
+# 检查 Docker 是否正在运行 Memos 容器
+if docker ps | grep -q "$CONTAINER_NAME"; then
+    log "Memos 容器正在运行，正在停止..."
+
+    # 判断是否是通过 docker-compose 启动的
+      if [ -f "$DOCKER_COMPOSE_FILE" ]; then
+           docker-compose down
+        if [[ $? -ne 0 ]]; then
+          log "停止 docker-compose 项目失败，请检查 Docker 日志。"
+          exit 1
+        fi
+      else
+            docker stop "$CONTAINER_NAME"
+              if [[ $? -ne 0 ]]; then
+                log "停止容器 $CONTAINER_NAME 失败，请检查 Docker 日志。"
+                 exit 1
+               fi
+      fi
+
+    log "Memos 容器已停止."
+else
+  log "Memos 容器未运行."
+fi
 # 进入本地仓库目录
 cd "$REPO_PATH" || {
-  echo "[$TIMESTAMP] Error: Could not change directory to $REPO_PATH" >> "$LOG_FILE"
+  log "Error: Could not change directory to $REPO_PATH"
   exit 1
 }
 
@@ -29,14 +58,15 @@ if ! git diff --cached --quiet --exit-code; then
   # 推送到远程仓库
     git push  >> "$LOG_FILE" 2>&1
   if [ $? -eq 0 ]; then
-       echo "[$TIMESTAMP] Backup successful!" >> "$LOG_FILE"
+      log "Backup successful!"
   else
-        echo "[$TIMESTAMP] Error: Backup failed, push failed." >> "$LOG_FILE"
+        log "Error: Backup failed, push failed."
+      exit 1
    fi
 
 else
-  echo "[$TIMESTAMP] No changes to commit." >> "$LOG_FILE"
+  log "No changes to commit."
 fi
 
-echo "[$TIMESTAMP] Backup process finished." >> "$LOG_FILE"
+log "Backup process finished."
 exit 0
